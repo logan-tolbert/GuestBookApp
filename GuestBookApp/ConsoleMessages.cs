7d74c06a47c65ffef224805968d025292530955a
logan-tolbert <logo88205+git@gmail.com>
2024-08-10 14:36:22 -0500

    Initial commit

---
+++ GuestBookApp/ConsoleMessages.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GuestBookApp
+{
+    internal static class ConsoleMessages
+    {
+        public static void AppIntro()
+        {
+            Console.WriteLine("   Charp GuestServices");
+            Console.WriteLine("  ---------------------");
+        }
+
+        public static string GetUserName()
+        {
+            
+            string message = "Enter your name: ";
+            string name = String.Empty;
+            bool getInput = true;
+            
+            do
+            {
+                Console.Write(message);
+                name = Console.ReadLine();
+                if (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+                else
+                {
+                    getInput = false;
+                }
+            } while (getInput);
+
+            return name;
+        }
+
+        public static void GreetUser(string userName)
+        {
+            string greeting = $"Hello, {userName}\n";
+            Console.Write(greeting);
+            Console.WriteLine("Press enter to start guest services.");
+            Console.ReadKey();
+            
+            Console.Clear();     
+        }
+
+        public static string GetGuestName()
+        {
+            bool getInput = true;
+            string welcomeGuest = "Welcome to the party!\n";
+            string getName = "Please enter your last name: ";
+            string lastName = "";
+            Console.Write(welcomeGuest);
+
+            do
+            {
+                Console.Write(getName);
+                lastName = Console.ReadLine();
+                
+                if (string.IsNullOrEmpty(getName))
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+                else
+                {
+                    getInput = false;
+                }
+            } while (getInput);
+            Console.Clear();
+            return lastName;
+        }
+
+        public static int GetNumberInParty()
+        {
+            string GetNumber = "Please enter the number of guest/guests in your party: ";
+            int NumberInParty;
+            bool isNumber = false;
+
+            do
+            {
+                Console.Write(GetNumber);
+                isNumber = int.TryParse(Console.ReadLine(), out NumberInParty);
+
+                if(isNumber)
+                {
+                    isNumber = true;
+                }
+            } while (isNumber == false);
+            Console.WriteLine("Enjoy the party!!!!");
+            return NumberInParty;
+
+
+        }
+    }
+}
---
+++ GuestBookApp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace GuestBookApp
+{
+    internal class Program
+    {
+        //Requirements
+        //Build a Console Guest Book App. 
+        //Ask the user for their name and how many are in their party.
+        //Keep track of how many people are in their party and at the party as a whole
+        //At the end, print out the quest list and the total number of guests
+        static void Main(string[] args)
+        {
+            // Introduce app
+            ConsoleMessages.AppIntro();
+
+            // Get users name
+            string userName = ConsoleMessages.GetUserName();
+            Console.Clear();
+
+            // Greet current user
+            ConsoleMessages.GreetUser(userName);
+
+            // Greet guests
+            // Ask for guest/guests name 
+            string guestName = ConsoleMessages.GetGuestName();
+
+            // Ask for number of guest/guests
+            int numberInParty = ConsoleMessages.GetNumberInParty();
+
+            // Check if more guests are coming
+
+            // Check list of names and print person/persons name the number in that party
+
+            // Print total number of guests that attended the party
+        }
+    }
+}
